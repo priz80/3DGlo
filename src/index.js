02bc1bde2bb8e3bf508b61a5e8bedef9004e0008
Igor <priz8090@gmail.com>
2026-09-25 15:19:57 +0300

    reg_expressions

--- src/index.js
+++ src/index.js
@@ -2,8 +2,10 @@ import timer from "./modules/timer";
 import menu from "./modules/menu";
 import modal from "./modules/modal";
 import scroll from "./modules/scroll";
+import calc from "./modules/calc";
 
 timer("17 october 2026");
 menu();
 modal();
 scroll();
+calc();
