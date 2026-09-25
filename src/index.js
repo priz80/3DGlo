2655fc2a23461920e7715ad24d27953c05568b66
Igor <priz8090@gmail.com>
2026-09-25 15:41:47 +0300

    reg_expressions

--- src/index.js
+++ src/index.js
@@ -3,9 +3,11 @@ import menu from "./modules/menu";
 import modal from "./modules/modal";
 import scroll from "./modules/scroll";
 import calc from "./modules/calc";
+import submit from "./modules/submit";
 
 timer("17 october 2026");
 menu();
 modal();
 scroll();
 calc();
+submit();
