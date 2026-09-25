db2098e5ce2176133c124437b3d93987d453c87f
Igor <priz8090@gmail.com>
2026-09-25 16:24:22 +0300

    reg_expressions

--- src/modules/calc.js
+++ src/modules/calc.js
@@ -1,16 +1,20 @@
 // calc.js
 
-const inputSquare = inputSquare.addEventListener("input", (e) => {
+const inputSquare = document.querySelector('.calc-square');
+const inputCount = document.querySelector('.calc-count');
+const inputDay = document.querySelector('.calc-day');
+
+inputSquare.addEventListener("input", (e) => {
   // Меняем значение (value) самого инпута, удаляя всё, кроме цифр
   inputSquare.value = inputSquare.value.replace(/[^\d]/g, '').slice(0, 4); // максимальное значение поля 9999
 });
 
-const inputCount = inputCount.addEventListener("input", (e) => {
+inputCount.addEventListener("input", (e) => {
   // Меняем значение (value) самого инпута, удаляя всё, кроме цифр
   inputCount.value = inputCount.value.replace(/[^\d]/g, '').slice(0, 2); // максимальное значение поля 99
 });
 
-const inputDay = inputDay.addEventListener("input", (e) => {
+inputDay.addEventListener("input", (e) => {
   // Меняем значение (value) самого инпута, удаляя всё, кроме цифр
   inputDay.value = inputDay.value.replace(/[^\d]/g, '').slice(0, 3); // максимальное значение поля 999
 });
